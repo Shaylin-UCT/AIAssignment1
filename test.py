73c2c5b76055af0beb093185f4e097a1f9460470
Shaylin-UCT <chtsha042@myuct.ac.za>
2023-05-04 10:16:11 +0200

    Added in method to iterate through complex nodes

--- test.py
+++ test.py
@@ -1,9 +1,31 @@
-#Just a piece of code to do random stuff needed
-listToChange = ["RadarFailure", "Fog", "EnvironmentalObstacles", "Visibility", "HighSpeed", "HumanFactor", "Navigation", "EquipmentError", "DetectionFailure", 
-             "RidgeIceAndIceberg", "PackIce", "HighWind", "Waves", "PackIceEffect", "NondetectedMultiLayerIce", "RidgeIceAndIcebergEffect", "WaveEffect", 
-             "FaultOfOtherVessels", "IceBreakersFailure", "DangerousIceCondition", "TypeOfExpedition", 'EnvironmentalOperationalEffects', "PotentialObstacles", "Collision"]
+import itertools
 
-for i in range(len(listToChange)):
-    listToChange[i] = listToChange[i].lower()
+def count(items):
+    num = 0
+    for i in items:
+        if i == 1:
+            num = num+1
+    if num<len(items)/2:
+        return True
+    else:
+        return False
 
-print(listToChange)
+def iterate(vars, mainVariable):
+    lst = list(itertools.product([0, 1], repeat=len(vars)))
+    items = {}
+    for i in lst:
+        for j in range(len(vars)):
+            items[vars[j]] = i[j]
+        #print(items)
+        if count(i):
+            print("Method insert for", mainVariable, "dictionary is", items, "with probabilities [1,0]")
+        else:
+            print("Method insert for",mainVariable, "dictionary is", items, "with probabilities [0,1]")
+
+def main():
+    vars = ["Fog", "Human", "Test"]
+    iterate(vars, "Navigation")
+
+
+if __name__=="__main__":
+    main()
