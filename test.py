2a662261cd34eb066e39b767f3cfb4d320d4df96
Shaylin-UCT <chtsha042@myuct.ac.za>
2023-05-08 12:45:36 +0200

    Changed Collision

--- test.py
+++ test.py
@@ -1,31 +1,10 @@
-import itertools
-
-def count(items):
-    num = 0
-    for i in items:
-        if i == 1:
-            num = num+1
-    if num<len(items)/2:
-        return True
+def even(num):
+    if num%2==0:
+        print("even")
     else:
-        return False
+        print("Odd")
 
-def iterate(vars, mainVariable):
-    lst = list(itertools.product([0, 1], repeat=len(vars)))
-    items = {}
-    for i in lst:
-        for j in range(len(vars)):
-            items[vars[j]] = i[j]
-        #print(items)
-        if count(i):
-            print("Method insert for", mainVariable, "dictionary is", items, "with probabilities [1,0]")
-        else:
-            print("Method insert for",mainVariable, "dictionary is", items, "with probabilities [0,1]")
+for i in range(0, 10):
+    print(i)
+    even(i)
 
-def main():
-    vars = ["Fog", "Human"]
-    iterate(vars, "Navigation")
-
-
-if __name__=="__main__":
-    main()
